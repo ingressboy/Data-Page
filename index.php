d6daf257e6ee0bc36bcd5b2d249cb3201e004fe3
ingressboy <81222444+ingressboy@users.noreply.github.com>
2021-11-12 14:31:23 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Data of SpielfoxLP-Cloud</title>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="Data, Log, Spielfox-Cloud">
+        <meta name="description" content="My Data of my Cloud">
+        <meta name="author" content="ingressboy">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h3>Minecraft Server = Port 25555</h3>
+        <a href="/mc-server-log/sky-attack/2021-11-09-1-log.php">2021-11-09-1.log</a>
+    </body>
+</html>
